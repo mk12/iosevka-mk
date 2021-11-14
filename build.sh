c0cf737c614c48a5e300b9a721ee6ffdbb4f8cae
Mitchell Kember <mk12360@gmail.com>
2021-11-13 18:04:30 -0800

    Uncomment things

--- build.sh
+++ build.sh
@@ -2,20 +2,17 @@
 
 set -eufo pipefail
 
-# readonly tmp=$(mktemp -d)
-# readonly repo="$tmp/Iosevka"
+readonly tmp=$(mktemp -d)
+readonly repo="$tmp/Iosevka"
 
-# cleanup() {
-#     rm -rf "$tmp"
-# }
-
-# # trap 'cleanup' EXIT
-
-# cd "$(dirname "$0")"
-# git clone --depth 1 git@github.com:be5invis/Iosevka "$repo"
-# cp iosevka-mk.toml "$repo/private-build-plans.toml"
-# npm -C "$repo" install
-# npm -C "$repo" run build -- ttf::iosevka-mk
-# mkdir -p ttf
-repo='/var/folders/9k/5tydddxn5rl7gt9qn5grls2r0000gn/T/tmp.aEshUoWf/Iosevka'
+cd "$(dirname "$0")"
+git clone --depth 1 git@github.com:be5invis/Iosevka "$repo"
+cp iosevka-mk.toml "$repo/private-build-plans.toml"
+npm -C "$repo" install
+npm -C "$repo" run build -- ttf::iosevka-mk
+mkdir -p ttf
 rsync -a --delete "$repo/dist/iosevka-mk/ttf/" ttf/
+
+echo
+echo "Finished building iosevka-mk."
+echo "To clean up, run: rm -rf $tmp"
